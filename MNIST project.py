b8d3ddbc82a6d94d28ec6f52fd06bd44ea913131
EslamElbassel <63676739+EslamElbassel@users.noreply.github.com>
2021-11-17 19:28:52 +0200

    Update MNIST project.py

--- MNIST project.py
+++ MNIST project.py
@@ -8,16 +8,17 @@ from sklearn import svm
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, r2_score, precision_score, recall_score
-print ("        *** Loading Data..... *** ")
+
 ################### Loading Data #################
+print ("        *** Loading Data..... *** ")
 (train_x, train_y) = loadlocal_mnist(images_path='/content/drive/MyDrive/train-images.idx3-ubyte', 
                                      labels_path='/content/drive/MyDrive/train-labels.idx1-ubyte')
 
 (test_x, test_y) = loadlocal_mnist(images_path='/content/drive/MyDrive/t10k-images.idx3-ubyte', 
                                    labels_path='/content/drive/MyDrive/t10k-labels.idx1-ubyte')
 
-print ("        *** Applaying HOG *** ")
 #####################  HOG  ####################
+print ("        *** Applaying HOG *** ")
 list_hog_fd = []
 list_hog_image = []
 for feature in train_x:
@@ -36,24 +37,26 @@ for test_feature in test_x:
   t_list_hog_fd.append(t_fd)
   t_list_hog_image.append(t_hog_image)
 t_hog_features = np.array(t_list_hog_fd)
+
 ####### plot sample of hogged images #######
 plt.axis("off")
 plt.imshow(list_hog_image[2], cmap="gray")
 plt.show()
-print ("        *** Fit & Test KNN *** ")
+
 ##################### KNN ##################
+print ("        *** Fit & Test KNN *** ")
 knn_model = KNeighborsClassifier(n_neighbors=10, metric='euclidean')
 knn_model.fit(hog_features, train_y)
 knn_prediction = knn_model.predict(t_hog_features)
 
-print ("        *** Fit & Test SVM *** ")
 ##################### SVM ##################  
+print ("        *** Fit & Test SVM *** ")
 svm_model = svm.SVC()
 svm_model.fit(hog_features, train_y)
 svm_prediction = svm_model.predict(t_hog_features)
 
+##################### ANN ##################
 print ("        *** Fit & Test ANN *** ")
-##################### ANN ################## 
 ann_model = Sequential()    
 ann_model.add(Dense(392, input_dim=392, activation='relu'))
 ann_model.add(Dense(56,activation='relu'))
@@ -67,8 +70,8 @@ ann_model.fit(hog_features, train_y, epochs=30)
 ann_prediction = ann_model.predict(t_hog_features)
 ann_prediction = np.argmax(ann_prediction, axis=1)
 
-print ("        *** Evaluation & Comparing models *** ")
 ############## Evaluation & Comparisons #####################
+print ("        *** Evaluation & Comparing models *** ")
 def evaluation_accuracies(labels_list, prediction_list):
   print("Accuracy_Score = ", accuracy_score(labels_list, prediction_list) * 100, "%")
   print("R2_score = ", r2_score(labels_list, prediction_list) * 100, "%")
